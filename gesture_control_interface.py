dd51f7d6150d5b7dbfb9e0390ffdc3891bb086de
Derendogann <derendogan26@gmail.com>
2024-12-10 09:37:31 +0300

    Update gesture_control_interface.py

--- gesture_control_interface.py
+++ gesture_control_interface.py
@@ -12,7 +12,7 @@ import os
 # Suppress Mediapipe warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-# MediaPipe settings
+# MediaPipe settings for hand tracking
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
